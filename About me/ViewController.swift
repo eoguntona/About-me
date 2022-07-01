314da861f655408a1cf9889b81ef006c81935d17
Elisabeth <elisa.oguntona@gmail.com>
2022-07-01 13:06:43 -0500

    Initial Commit

---
+++ About me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Scholar on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Label1: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
